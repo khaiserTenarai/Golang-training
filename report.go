73e07493a3b8b4392ee33d2b95a22c5caa511192
Piyush Goyal <Piyush.Goyal@tenarai.com>
2026-09-23 16:47:42 +0530

    ADD employee report functionality

--- report.go
+++ report.go
@@ -1,1 +1,12 @@
-package employeemanagement
+package main
+
+import "fmt"
+
+func GenerateReport(employees []Employee) {
+	fmt.Println("Employee Report")
+	fmt.Println("----------------")
+
+	for _, employee := range employees {
+		fmt.Println(employee.Name, "-", employee.Department)
+	}
+}
