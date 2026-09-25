ae70ff291af266a14e09653dc39076c03f56fd00
Pooja M K <Pooja1.K@infogain.com>
2026-09-25 13:39:36 +0530

    Clean up old files and add Pooja's Day 1 and Day 2 assignment folders

--- Day1Piyush/go-day1/employeeapp/pkg/utils/utils.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-// CalculateAnnualSalary calculates an employee's annual salary
-// from their monthly salary.
-func CalculateAnnualSalary(monthlySalary float64) float64 {
-	return monthlySalary * 12
-}
